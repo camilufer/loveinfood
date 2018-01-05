81519ff76952b58245ea5e091c1aedf6b605f7e6
camilufer <camilu.fer21@gmail.com>
2018-01-05 01:01:52 -0300

    mas jquery

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
 	$('#comentar').click(function(){
 		$('#oculto').show();
-	
+	$('#recetas, #amigos').click(function(){
+		$('#oculto').hide();
 	});
+
+	});
+
+	$('#recetas').click(function(){
+		$('#escondido').show();
+	$('#comentar, #amigos').click(function(){
+		$('#escondido').hide();
+	});
+   });
 
 });
